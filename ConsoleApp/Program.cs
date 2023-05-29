ee18ea93b91628fca2fdf45a7d92455f624c7aa6
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-29 10:45:57 +0200

    Wprowadzenie do .NET 6

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//namespace ConsoleApp
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//Instrukcja najwyższego poziomu
+
+
+Console.WriteLine($"Hello, {args[0]}!");
+JsonConvert.SerializeObject(new object());
+
+string? name = "ALA";
+string result = ToLower(name)!; // ! - uspokaja warningi przy niezgodności typu NULL z nieNULL
+
+string? ToLower(string? value /*!! - parameter null-checking feature - automatycznie dodaje kod jak poniżej*/)
+{
+    /*if(value == null)
+        throw new ArgumentNullException("value");*/
+
+    return value?.ToLower();
+}
+
+
+
+//        }
+//    }
+//}
